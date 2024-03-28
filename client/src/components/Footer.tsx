bb21eca634031a86c827c8c11da1c9f940ff563e
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-28 13:19:13 +0530

    footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <div className="bg-purple-700 py-6">
-            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-                <span className="text-3xl text-white tracking-tighter font-bold">
+            <div className="container mx-auto flex flex-row justify-between items-center">
+                <span className="text-2xl text-white tracking-tighter font-bold">
                     TigerPay &copy; 2024
                 </span>
                 <span className="text-white font-bold tracking-tighter flex gap-4">
